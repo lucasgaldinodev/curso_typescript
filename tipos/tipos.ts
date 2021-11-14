e801117f45f41b0a8e55bf177f859f67fc250d22
Lucas Galdino <lucasgaldino7@gmail.com>
2021-11-14 00:16:28 -0300

    12/NOV

--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -28,3 +28,64 @@ console.log(typeof hobbies)
 hobbies = [100, 200, 300]
 // hobbies = 100
 console.log(hobbies)
+
+// tuplas
+let endereco: [string, number, string] = ["Av Principal", 99, ""]
+console.log(endereco)
+
+endereco = ["Rua Importante", 1260, "Bloco C"]
+console.log(endereco)
+
+// enums
+enum Cor {
+    Cinza, // 0
+    Verde = 100, // 100
+    Azul = 10, // 2
+    Laranja,
+    Amarelo,
+    Vermelho = 100
+}
+
+let minhaCor: Cor = Cor.Verde
+console.log(minhaCor)
+
+console.log(Cor.Azul)
+console.log(Cor.Laranja, Cor.Amarelo)
+console.log(Cor.Verde, Cor.Vermelho)
+
+// any
+let carro: any = 'BMW'
+console.log(carro)
+carro = { marca: 'BMW', ano: 2019 }
+console.log(carro)
+
+// funções
+function retornaMeuNome(): string {
+    // return minhaIdade
+    return nome
+}
+
+console.log(retornaMeuNome())
+
+function digaOi(): void {
+    console.log('Oi')
+    // return minhaIdade
+}
+
+digaOi()
+
+function multiplicar(numA: any, numB: any): number {
+    return numA * numB
+}
+
+// console.log(multiplicar(2, 'Bia'))
+console.log(multiplicar(4.7, 9))
+
+// tipo função
+let calculo: (x: number, y: number) => number
+// calculo = digaOi
+// calculo()
+// calculo = {}
+
+calculo = multiplicar
+console.log(calculo(5, 6))
